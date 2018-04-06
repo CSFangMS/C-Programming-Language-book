7e6c70b053d4f830d1f541c298fdf6e3e8275bdb
Stanley Fang <stfang925@gmail.com>
2018-04-07 00:00:57 +0800

    Updates
    fresh start

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # C-Programming-Language
+
 C語言教學手冊-洪維恩
+實作紀錄
+
+## Reference
+
+[Visual Studio Code with C++](https://ithelp.ithome.com.tw/articles/10190235)
---
+++ prog1_1.c
@@ -0,0 +1,12 @@
+/* prog1_1, My first C code */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Hello C!\n");            /* print Hello C! string */
+    printf("Hello World!\n");        /* print Hello World! string */
+
+    system("pause");
+    return 0;
+    
+}
